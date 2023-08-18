34e4fd94404a8f07d93836a1bf792096f705da8c
Akujin25 <106136034+Akujin25@users.noreply.github.com>
2023-08-18 09:25:35 +0700

    Api all

---
+++ backend/ManageStorage/Controllers/InputStorageController.cs
@@ -0,0 +1,96 @@
+﻿using ManageStorage.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace ManageStorage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InputStorageController : ControllerBase
+    {
+        private QuanlykhoContext _context;
+        public InputStorageController(QuanlykhoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public IActionResult List()
+        {
+            return Ok(_context.InputStorages.ToList());
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(InputStorage input)
+        {
+            try
+            {
+                _context.InputStorages.Add(input);
+                int result = _context.SaveChanges();
+                return Ok(input);
+
+            }
+            catch
+            {
+                return Conflict();
+            }
+        }
+
+        [HttpPut("edit/{id}")]
+        public IActionResult EditUser(InputStorage input, int id)
+        {
+            try
+            {
+                var us = _context.InputStorages.FirstOrDefault(o => o.InputId == id);
+
+                if (us is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    us.ProductId = input.ProductId;
+                    us.Quantity = input.Quantity;
+                    us.DateInput = input.DateInput;
+                    us.InputPrice = input.InputPrice;
+                    us.OutputPrice = input.OutputPrice;
+                    us.Status = input.Status;
+
+                    int result = _context.SaveChanges();
+                    return Ok(result);
+                }
+            }
+            catch
+            {
+                return Conflict();
+            }
+        }
+
+        [HttpPut("delete")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                InputStorage input = _context.InputStorages.FirstOrDefault(u => u.InputId == id);
+
+                if (input is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    input.Status = false;
+
+                    int result = _context.SaveChanges();
+                    return Ok(result);
+                }
+            }
+            catch
+            {
+                return Conflict();
+            }
+        }
+    }
+}
--- backend/ManageStorage/Controllers/UserController.cs
+++ backend/ManageStorage/Controllers/UserController.cs
@@ -2,6 +2,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Query;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ManageStorage.Controllers
 {
@@ -10,9 +15,11 @@ namespace ManageStorage.Controllers
     public class UserController : ControllerBase
     {
         private QuanlykhoContext _context;
-        public UserController(QuanlykhoContext context)
+        public IConfiguration _configuration;
+        public UserController(IConfiguration configuration, QuanlykhoContext context)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         [HttpGet]
@@ -90,6 +97,52 @@ namespace ManageStorage.Controllers
             {
                 return Conflict();
             }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Post(User _userData)
+        {
+            if (_userData != null && _userData.Username != null && _userData.Password != null)
+            {
+                var user = await GetUser(_userData.Username, _userData.Password);
+
+                if (user != null)
+                {
+                    //create claims details based on the user information
+                    var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub,user.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("UserId", user.UserId.ToString()),
+                        new Claim("DisplayName", user.DisplayName),
+                        new Claim("Role", user.Role),
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddMinutes(10),
+                        signingCredentials: signIn);
+
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private async Task<User> GetUser(string username, string password)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
         }
     }
 }
