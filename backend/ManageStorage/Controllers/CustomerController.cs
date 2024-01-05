5de7a1c7933cca6948e1f7a84e69e75b1e7b4f4b
dmanhlam <97490097+dmanhlam3101@users.noreply.github.com>
2024-01-05 20:52:36 +0700

    ss
    sd

--- backend/ManageStorage/Controllers/CustomerController.cs
+++ backend/ManageStorage/Controllers/CustomerController.cs
@@ -25,7 +25,7 @@ namespace ManageStorage.Controllers
             {
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
             };
-
+    int i =0;
             var customers = _context.Customers.Include(o => o.OutputStorages).Where(o => o.Status != false).ToList();
             var json = JsonConvert.SerializeObject(customers, jsonSettings);
 
